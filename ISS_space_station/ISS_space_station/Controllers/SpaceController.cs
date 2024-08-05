fe2995feb4689f45a6036f8b9c2b2ebb3589e11d
micha <micha@KOMPUTER>
2024-08-05 21:54:08 +0200

    Init oraz dodanie mapy

---
+++ ISS_space_station/ISS_space_station/Controllers/SpaceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ISS_space_station.Controllers
+{
+    public class SpaceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(); 
+        }
+    }
+}
